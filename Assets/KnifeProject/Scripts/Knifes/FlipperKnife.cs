8b114796b593d5b5ddda3c354dccbddd7d1d3d0e
RuNa2048 <41949451+RuNa2048@users.noreply.github.com>
2021-11-23 20:18:21 +0300

    Fix bug jump

--- Assets/KnifeProject/Scripts/Knifes/FlipperKnife.cs
+++ Assets/KnifeProject/Scripts/Knifes/FlipperKnife.cs
@@ -6,32 +6,47 @@ public class FlipperKnife : MonoBehaviour
 {
 	[Header("Physics parametrs")]
 	[SerializeField] private float _kickForce = 5f;
-	[SerializeField] private float _kickScale = 1000f;
 	[SerializeField] private float _torqueForce = 20f;
 
+	[Header("Timings")]
+	[SerializeField] private float _detectorPauseTime = 0.5f;
+
+
+	private float _discardingTime;
+
 	private Rigidbody _rigidbody;
+	private SphereCollider _platformDetector;
+
 
 	private void Awake()
 	{
 		_rigidbody = GetComponent<Rigidbody>();
+		_platformDetector = GetComponent<SphereCollider>();
 	}
 
 	public void Discarding(Vector2 force)
 	{
+		StartCoroutine(DetectorDelay());
+
+		_discardingTime = Time.time;
 		_rigidbody.isKinematic = false;
 
 		_rigidbody.AddForce(force * _kickForce, ForceMode.Impulse);
 		_rigidbody.AddTorque(_torqueForce, 0f, 0f, ForceMode.Impulse);
 	}
 
+	private IEnumerator DetectorDelay()
+	{
+		_platformDetector.enabled = false;
+
+		yield return new WaitForSeconds(_detectorPauseTime);
+
+		_platformDetector.enabled = true;
+	}
+
 	private void OnTriggerEnter(Collider other)
 	{
-		if (!_rigidbody.isKinematic)
-		{
-			Debug.Log("Trigger");
-
-			_rigidbody.isKinematic = true;
-		}
+		_rigidbody.isKinematic = true;
 	}
 
 	private void OnCollisionEnter(Collision collision)
