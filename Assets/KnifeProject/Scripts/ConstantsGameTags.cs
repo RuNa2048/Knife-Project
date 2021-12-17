ac69633331565172328d2afc19983a8b11b11d1b
RuNa2048 <41949451+RuNa2048@users.noreply.github.com>
2021-12-18 00:08:43 +0300

    Jump Mechanic And Checkpoints v1.2

---
+++ Assets/KnifeProject/Scripts/ConstantsGameTags.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConstantsGameTags
+{
+    public const string PlayerTextTag = "Player";
+    public const string PlatformTextTag = "Platform";
+    public const string PlatformWallTextTag = "PlatformWall";
+}
--- Assets/KnifeProject/Scripts/Platform.cs
+++ Assets/KnifeProject/Scripts/Platform.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class Platform: MonoBehaviour
 {
 	[Header("Player Settings")]
-	[SerializeField] private string _playerTag = "Player";
 	[SerializeField] private Transform _checkpointPos;
 
 	[Header("Treatment Checkpoints")]
 	[SerializeField] private Checkpoint _checkpoint;
 
+	private string _playerTag;
+
+	private void Start()
+	{
+		_playerTag = ConstantsGameTags.PlayerTextTag;
+	}
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == _playerTag)
 		{
-			_checkpoint.SavingPosition(_checkpointPos.position);
+			_checkpoint.SavingPosition(_checkpointPos);
 		}
 	}
 }
